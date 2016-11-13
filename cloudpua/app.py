548c13bac57ee922ab6a35e2be836c864dff857d
AZLisme <helloazl@icloud.com>
2016-11-14 00:09:46 +0800

    添加配置文件、整理页面代码

--- cloudpua/app.py
+++ cloudpua/app.py
@@ -7,6 +7,7 @@ from .db import configure as db_configure
 def create_app():
     """create the flask app"""
     app = Flask("cloudpua")
+    app.config.from_pyfile('settings.py')
     db_configure(app)
     from .url import url_blueprint
     app.register_blueprint(url_blueprint)
---
+++ cloudpua/settings.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+"""Configure app"""
+
+TEMPLATES_AUTO_RELOAD = True
